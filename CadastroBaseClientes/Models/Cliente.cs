c6d0d53aedcf5315e14395aea4aff7d1d06dc865
GUSTAVO ALVES ZACCHEU <081230023@faculdade.cefsa.edu.br>
2024-03-01 19:18:07 -0300

    Criação classe cliente

---
+++ CadastroBaseClientes/Models/Cliente.cs
@@ -0,0 +1,8 @@
+﻿public class Cliente
+{
+    public int ID { get; set; }
+    public string Nome { get; set; }
+    public char Sexo { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
